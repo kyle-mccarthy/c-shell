fe4ea8ab08ae51a68fa89f4a9fe4c16e14c5562a
Kyle McCarthy <km@kylemccarthy.io>
2015-05-07 14:32:41 -0500

    basic functionality structure

--- src/shell_utils.h
+++ src/shell_utils.h
@@ -8,9 +8,30 @@
 #ifndef _SHELL_UTILS_H_
 #define _SHELL_UTILS_H_
 
+/**
+ *
+ * @return 
+ */
 int pwd();
 
+/**
+ *
+ * @return
+ */ 
+ int cd();
+
+/**
+ *
+ * @return
+ */
 int ls();
+
+ /**
+  *
+  * @return
+  */
+
+int exec();
 
 
 

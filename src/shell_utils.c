18cf78648ea4c286f39d5ef1b40b898edff74099
mpshos <mps9vf@gmail.com>
2015-05-07 08:51:56 -0500

    uploaded the parser he gave use and created some utility source files

---
+++ src/shell_utils.c
@@ -0,0 +1,12 @@
+#include "shell_utils.c"
+#include <dirent.h>
+#include <sys/types.h>
+
+
+int pwd(){
+	return 0;
+}
+
+int ls(){
+	return 0;
+}
---
+++ src/shell_utils.h
@@ -0,0 +1,19 @@
+/*
+	This file contains the declaration of the built in commands and other
+	miscellaneous functions we might need. It might not be necessary if there
+	aren't many
+
+*/
+
+#ifndef _SHELL_UTILS_H_
+#define _SHELL_UTILS_H_
+
+int pwd();
+
+int ls();
+
+
+
+
+
+#endif

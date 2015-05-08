6ebc98b1aa3698e6dc5708a474e2e2c29ec9ba01
Kyle McCarthy <km@kylemccarthy.io>
2015-05-08 13:04:50 -0500

    fixed weird spacing issues with tab char vs 4 spaces

--- src/osShell.c
+++ src/osShell.c
@@ -1,7 +1,7 @@
 /**
-  osShell - parses user input and executes commands
+osShell - parses user input and executes commands
 
-  Build:    gcc read_token_to_array.c -o osShell -lreadline   
+Build:    gcc read_token_to_array.c -o osShell -lreadline   
 */
 #include "../include/shell_utils.h"
 #include <string.h>
@@ -21,87 +21,87 @@ int main(int argc, char * argv[])
    char indirectionOp;
    int indirectionOpIndx, errCode;
 
-// readline  will  read a line from the terminal and return it, using prompt as a prompt.  If prompt is NULL or the empty string, no
-// prompt is issued.  The line returned is allocated with malloc(3); the caller must free it when finished.  The line  returned  has
-// the final newline removed, so only the text of the line remains.
+   // readline  will  read a line from the terminal and return it, using prompt as a prompt.  If prompt is NULL or the empty string, no
+   // prompt is issued.  The line returned is allocated with malloc(3); the caller must free it when finished.  The line  returned  has
+   // the final newline removed, so only the text of the line remains.
    cmdBuffer = readline("osShell >$ ");
 
-
-	// WHILE NOT EXIT
+   // WHILE NOT EXIT
    while ( 0 != strncmp("EXIT", cmdBuffer, 4))
    {
-	// Our Token Parser
-   	unsigned char currentTokenIndex = 0;
-   	//does stdin or stdout need to be rerouted
-   	indirectionOp = '\0';
+      // Our Token Parser
+      unsigned char currentTokenIndex = 0;
+      //does stdin or stdout need to be rerouted
+      indirectionOp = '\0';
 
-	// Get some heap to copy into so it is scoped proper for passes and forks
-	heapBuffer = (char *) calloc(strlen(cmdBuffer), sizeof(char));
-	strncpy(heapBuffer,cmdBuffer, strlen(cmdBuffer) * sizeof(char));
+      // Get some heap to copy into so it is scoped proper for passes and forks
+      heapBuffer = (char *) calloc(strlen(cmdBuffer), sizeof(char));
+      strncpy(heapBuffer,cmdBuffer, strlen(cmdBuffer) * sizeof(char));
 
-	/* get the first token */
-	myArgv[ currentTokenIndex ] = strtok(heapBuffer, " ");
+      /* get the first token */
+      myArgv[ currentTokenIndex ] = strtok(heapBuffer, " ");
 
-	/* walk through other tokens */
-	while( myArgv[ currentTokenIndex ] != NULL && currentTokenIndex < 98) 
-	{
+      /* walk through other tokens */
+      while( myArgv[ currentTokenIndex ] != NULL && currentTokenIndex < 98) 
+      {
 
-	   //printf( "%d] %s\n", currentTokenIndex, myArgv[ currentTokenIndex ] );
+         //printf( "%d] %s\n", currentTokenIndex, myArgv[ currentTokenIndex ] );
 
-		//determine if one of the indirection operators is used 
-		if (strncmp(myArgv[currentTokenIndex], "|", 2) == 0){
-			indirectionOp = '|';
-			indirectionOpIndx = currentTokenIndex;
-		}
-		else if (strncmp(myArgv[currentTokenIndex], ">", 2) == 0){
-			indirectionOp = '>';
-			indirectionOpIndx = currentTokenIndex;
-		}
+         //determine if one of the indirection operators is used 
+         if (strncmp(myArgv[currentTokenIndex], "|", 2) == 0) {
+            indirectionOp = '|';
+            indirectionOpIndx = currentTokenIndex;
+         }
+         else if (strncmp(myArgv[currentTokenIndex], ">", 2) == 0) {
+            indirectionOp = '>';
+            indirectionOpIndx = currentTokenIndex;
+         }
 
-		currentTokenIndex ++;
+         currentTokenIndex ++;
 
-	    
-		myArgv[ currentTokenIndex ] = strtok(NULL, " ");
-	}
+         myArgv[ currentTokenIndex ] = strtok(NULL, " ");
+      }
 
-	myArgv[currentTokenIndex + 1] = "\0"; // Null Term Arg Array
+      myArgv[currentTokenIndex + 1] = "\0"; // Null Term Arg Array
 
-   	//printf("Number of Tokens: %d \n ", currentTokenIndex);
+      //printf("Number of Tokens: %d \n ", currentTokenIndex);
 
-   	if (!validOp(myArgv[0])){
-   		printf("osShell >$Invalid Operation1");
-   		continue;
-   	}
+      if (!validOp(myArgv[0])){
+         printf("osShell >$Invalid Operation1");
+         continue;
+      }
 
-	// Now do something with an array like exec
-   	if (indirectionOp != '\0'){
-   		if (indirectionOp == '|' && !validOp(myArgv[indirectionOpIndx + 1])){
-     		   printf("osShell >$Invalid Operation2");
-   			continue; 			
-   		}
-   		if (indirectionOp == '|'){
-   			errCode = runCommandWithPipe(myArgv[0], indirectionOpIndx -1, myArgv + 1, myArgv[indirectionOpIndx + 1], currentTokenIndex - indirectionOpIndx, myArgv + indirectionOpIndx + 1);
+      // Now do something with an array like exec
+      if (indirectionOp != '\0') {
+         if (indirectionOp == '|' && !validOp(myArgv[indirectionOpIndx + 1])) {
+            printf("osShell >$Invalid Operation2");
+            continue; 			
+         }
+         if (indirectionOp == '|'){
+            errCode = runCommandWithPipe(myArgv[0], 
+               indirectionOpIndx -1, myArgv + 1, myArgv[indirectionOpIndx + 1],
+                currentTokenIndex - indirectionOpIndx, myArgv + indirectionOpIndx + 1);
+         }
+         else if (indirectionOp  == '>'){
+            errCode = runCommandWithOutputRedirect(myArgv[0], 
+               indirectionOpIndx - 1, myArgv + 1, myArgv[indirectionOpIndx + 1]);
+         }
+      }
+      else{
+         if (strncmp(myArgv[0], "cd", 3) == 0){
+            errCode = cd(myArgv[1]);
+         }
+         else{
+            errCode = runCommand(myArgv[0], currentTokenIndex - 1,  myArgv + 1);
+         }
+      }
 
-   		}
-   		else if (indirectionOp  == '>'){
-   			errCode = runCommandWithOutputRedirect(myArgv[0],indirectionOpIndx - 1, myArgv + 1, myArgv[indirectionOpIndx + 1]);
-   		}
-   	}
-   	else{
-   		if (strncmp(myArgv[0], "cd", 3) == 0){
-   			errCode = cd(myArgv[1]);
-   		}
-   		else{
-   			errCode = runCommand(myArgv[0], currentTokenIndex - 1,  myArgv + 1);
-   		}
-   	}
-
-	free( heapBuffer );
-	free( cmdBuffer );
-	// Read the next command
-	cmdBuffer = readline("osShell >$ ");
+      free( heapBuffer );
+      free( cmdBuffer );
+      // Read the next command
+      cmdBuffer = readline("osShell >$ ");
    }
-	
+
    free( cmdBuffer );
 
    return(0);
